950860e18700ae3fa716ccc3584acb02d8811cce
RA2211003010031 <ar6893@srmist.edu.in>
2025-12-15 08:31:49 +0530

    1st day

---
+++ Firstclass.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Firstclass{
+    public static void main(String args[]) {
+        // System.out.println("*");
+        // System.out.println("* *");
+        // System.out.println("* * *");
+        // System.out.println("* * * *");
+
+
+
+
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+
+        sc.close();
+
+    }
+}
