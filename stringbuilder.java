a05dd17c6e46c32c36f4915b26f969e2d58ffa01
RA2211003010031 <ar6893@srmist.edu.in>
2025-12-16 20:05:22 +0530

    Add string manipulation functionality with StringBuilder and Scanner

---
+++ stringbuilder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class stringbuilder{
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("");
+
+        sb.append("Adarsh Raj");
+
+        System.out.println(sb);
+
+    }
+}
