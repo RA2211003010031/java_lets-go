af04bae6b1a02f6caf3d4b687a8e3ac60d40f419
RA2211003010031 <ar6893@srmist.edu.in>
2026-01-07 09:52:56 +0530

    understanding basics of enums v1

---
+++ enums.java
@@ -0,0 +1,35 @@
+//enumeration - enum
+
+enum Status 
+{
+    Success, Failed, Pending, Running;
+}
+
+public class enums {
+    
+    public static void main()
+    {
+
+        Status s = Status.Success;
+        System.out.println(s);
+
+        //also all of these objects of STATUS are having numbering system also
+        System.out.println(Status.Failed.ordinal());        //ordinal is used to get the number order of object.   
+
+
+        //also we can use loops to check all at once or print all at once
+        Status[] ss = Status.values();      //putting all status objects in the array ss
+        for(Status st : ss) 
+        {
+            System.out.println(st + ": " + st.ordinal());       //getting objects one by one with their ordering values
+        }
+
+
+
+
+
+
+
+    }
+
+}
