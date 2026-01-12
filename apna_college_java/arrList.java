0d572d19d83edddb3a1405129bdbb1a25b04b39c
RA2211003010031 <ar6893@srmist.edu.in>
2026-01-12 11:47:22 +0530

    understading array List in Java

---
+++ apna_college_java/arrList.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class arrList {
+    
+    public static void main(String[] args) 
+    {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        //or can be defined like 
+        //ArrayList<Integer> list = new ArrayList<Integer>();
+
+        //add element
+        list.add(0);
+        list.add(1);
+
+        System.out.println(list);
+
+        //get element
+        int element = list.get(0);
+        System.out.println(element);
+
+        //add elem in btween
+        list.add(1,2);
+        System.out.println(list);
+
+        //set element
+        list.set(0, 1);
+        System.out.println(list);
+
+
+        //delete element
+        list.remove(2);
+        System.out.println(list);
+
+        //size check
+        int n = list.size();
+        System.out.println(n);
+
+        //loop
+        for(int i : list) {
+            System.out.println(i);
+        }
+
+
+        //sorting
+        list.add(2, 0);
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
+
+}
